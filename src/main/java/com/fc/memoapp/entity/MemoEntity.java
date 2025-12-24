6c08db6a4e67af4e40e7bcb928e592588655f569
for <ykin1031@gmail.com>
2025-12-24 15:32:45 +0900

    バリデーションを追加する。pomに依存関係を追加して@sizeとかを使えるようにした。

--- src/main/java/com/fc/memoapp/entity/MemoEntity.java
+++ src/main/java/com/fc/memoapp/entity/MemoEntity.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class MemoEntity {
@@ -11,8 +13,11 @@ public class MemoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @NotBlank
+    @Size(max = 20)
     private String title;
+    @NotBlank
+    @Size(max = 200)
     private String content;
 
     public MemoEntity() {}
