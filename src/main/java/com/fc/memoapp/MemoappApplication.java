d18c27fd334c43393b8a20296a3b3431a907d0a6
for <ykin1031@gmail.com>
2025-12-30 06:10:00 +0900

    更新日をindex.htmlに追加した

--- src/main/java/com/fc/memoapp/MemoappApplication.java
+++ src/main/java/com/fc/memoapp/MemoappApplication.java
@@ -2,8 +2,10 @@ package com.fc.memoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing 
 public class MemoappApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/fc/memoapp/entity/MemoEntity.java
+++ src/main/java/com/fc/memoapp/entity/MemoEntity.java
@@ -1,15 +1,32 @@
 package com.fc.memoapp.entity;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 @Entity
+@EntityListeners(AuditingEntityListener.class) 
 public class MemoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
+    
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt; // 作成日時
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt; // 更新日時
+    
     public MemoEntity() {
     }
     public MemoEntity(String title, String content) {
@@ -34,4 +51,9 @@ public class MemoEntity {
     public void setContent(String content) {
         this.content = content;
     }
+    
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+    public LocalDateTime getUpdatedAt() { return updatedAt; }
+    public void setUpdatedAt(LocalDateTime updatedAt) { this.updatedAt = updatedAt; }
 }
